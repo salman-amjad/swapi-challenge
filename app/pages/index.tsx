b570e93652960a90c03d85c43750cbee30d49146
Salman <salmanamjad89@gmail.com>
2021-04-27 20:25:31 +1000

    Home page set to private

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -265,7 +265,7 @@ const Home: BlitzPage = () => {
   )
 }
 
-Home.suppressFirstRenderFlicker = true
+Home.authenticate = { redirectTo: Routes.LoginPage().pathname }
 Home.getLayout = (page) => <Layout title="Home">{page}</Layout>
 
 export default Home
